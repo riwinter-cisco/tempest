cdac7b0deab417146e622a2363f22593f430c8b8
Richard Winters <riwinter@cisco.com>
2015-01-30 11:39:50 -0500

    Added -w flag to ping host cmd

--- tempest/common/utils/linux/remote_client.py
+++ tempest/common/utils/linux/remote_client.py
@@ -90,7 +90,7 @@ class RemoteClient():
     def ping_host(self, host, count=1, size=56):
         addr = netaddr.IPAddress(host)
         cmd = 'ping6' if addr.version == 6 else 'ping'
-        cmd += ' -c{0} -s{1} {2}'.format(count, size, host)
+        cmd += ' -c{0} -w{0} -s{1} {2}'.format(count, size, host)
         return self.exec_command(cmd)
 
     def get_mac_address(self):
